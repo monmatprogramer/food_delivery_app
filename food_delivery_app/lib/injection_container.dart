00406dd03e10046bf2fbd48720b1aea769bfe387
monmatprogrammer <monsokmeanmon@gmail.com>
2025-03-03 06:40:34 +0700

    something change

--- food_delivery_app/lib/injection_container.dart
+++ food_delivery_app/lib/injection_container.dart
@@ -1,10 +1,24 @@
+import 'package:dio/dio.dart';
 import 'package:food_delivery_app/core/contants/app_constants.dart';
+import 'package:food_delivery_app/core/network/dio_client.dart';
+import 'package:food_delivery_app/core/network/network_info.dart';
+import 'package:food_delivery_app/data/datasources/local/restaurant_local_data_source.dart';
+import 'package:food_delivery_app/data/datasources/restaurant_remote_data_source.dart';
 import 'package:food_delivery_app/data/models/category_model.dart';
 import 'package:food_delivery_app/data/models/restaurant_model.dart';
 import 'package:food_delivery_app/data/models/tag_model.dart';
+import 'package:food_delivery_app/domain/reponsitories/restaurant_repository.dart';
+import 'package:food_delivery_app/domain/reponsitories/restaurant_repository_impl.dart';
+import 'package:food_delivery_app/domain/usecases/get_categories.dart';
+import 'package:food_delivery_app/domain/usecases/get_featured_restaurants.dart';
+import 'package:food_delivery_app/domain/usecases/get_restaurants.dart';
+import 'package:food_delivery_app/domain/usecases/get_restaurants_by_category.dart';
 import 'package:food_delivery_app/presentation/bloc/restaurant/restaurant_bloc.dart';
+import 'package:food_delivery_app/presentation/bloc/restaurant/restaurant_state.dart';
 import 'package:get_it/get_it.dart';
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:logger/logger.dart';
 
 /// Service locator instance
 final sl = GetIt.instance;
@@ -26,8 +40,62 @@ Future<void> init() async {
       await Hive.openBox<CategoryModel>(AppConstants.categoriesBox);
 
   // BloCs
-  sl.registerFactory(() => RestaurantBloc(
+  sl.registerFactory(
+    () => RestaurantBloc(
       getRestaurants: sl(),
       getFeaturedRestaurants: sl(),
-      getRestaurantsByCategory: sl()));
+      getRestaurantsByCategory: sl(),
+    ),
+  );
+
+  //Use case
+  sl.registerLazySingleton(() => GetRestaurants(sl()));
+  sl.registerLazySingleton(() => GetFeaturedRestaurants(sl()));
+  sl.registerLazySingleton(() => GetRestaurantsByCategory(sl()));
+  sl.registerLazySingleton(() => GetCategories(sl()));
+
+  // Repository
+  sl.registerLazySingleton<RestaurantRepository>(
+    () => RestaurantRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  //Data source
+  sl.registerLazySingleton<RestaurantRemoteDataSource>(
+      () => RestaurantRemoteDataSourceImpl(client: sl()));
+  sl.registerLazySingleton<RestaurantLocalDataSourceImpl>(
+    () => RestaurantLocalDataSourceImpl(
+      restaurantBox: sl(),
+      categoriesBox: sl(),
+    ),
+  );
+
+  // Core
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+
+  sl.registerLazySingleton(
+    () => ApiClient(
+      dio: sl(),
+      logger: sl(),
+    ),
+  );
+
+  // External
+  sl.registerLazySingleton(() => Dio());
+  sl.registerLazySingleton(
+    () => Logger(
+      printer: PrettyPrinter(
+        methodCount: 0,
+        errorMethodCount: 5,
+        lineLength: 75,
+        colors: true,
+        printEmojis: true,
+        printTime: false,
+      ),
+    ),
+  );
+  sl.registerLazySingleton(() => InternetConnectionChecker.createInstance());
 }
--- food_delivery_app/lib/main.dart
+++ food_delivery_app/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:food_delivery_app/presentation/bloc/restaurant/restaurant_bloc.dart';
+import 'injection_container.dart' as di;
 import 'package:food_delivery_app/presentation/pages/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const MyApp());
 }
 
@@ -13,15 +16,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Food delivery app',
-      theme: ThemeData(
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: HomePage()
+    return MultiBlocProvider(
+      providers: [
+        // Provide restaurant BloC
+
+        BlocProvider<RestaurantBloc>(
+          create: (_) => di.sl<RestaurantBloc>(),
+        ),
+        //TODO: Main App File : Provide category BLoC
+      ],
+      child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Food delivery app',
+          theme: ThemeData(
+            // tested with just a hot reload.
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: HomePage()),
     );
   }
 }
